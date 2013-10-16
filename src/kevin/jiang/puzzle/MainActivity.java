56ce3c73b0c49cccf8c00931b496cb83c8b0d873
Kevin Jiang <kittymiky@gmail.com>
2013-10-16 23:47:38 +0800

    complete but can not puzzle

--- src/kevin/jiang/puzzle/MainActivity.java
+++ src/kevin/jiang/puzzle/MainActivity.java
@@ -1,22 +1,24 @@
 package kevin.jiang.puzzle;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.res.Resources;
+//import android.content.res.Resources;
 import android.util.Log;
-import android.view.Menu;
+//import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
+//import android.widget.LinearLayout;
 //import android.view.Window;
 
 public class MainActivity extends Activity implements OnTouchListener {
-	private int[] imbtns = {R.id.imageButton1, R.id.imageButton2, R.id.imageButton3, 
-    		R.id.imageButton4, R.id.imageButton5, R.id.imageButton6, 
-    		R.id.imageButton7, R.id.imageButton8, R.id.imageButton9, 
-    		R.id.imageButton10, R.id.imageButton11, R.id.imageButton12};
+	
+	private float xDown, yDown;
+	private boolean isShuffle = false;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,30 +29,79 @@ public class MainActivity extends Activity implements OnTouchListener {
 
         // 设置事件监听
         setEventListene();
-        
+        shuffle();
     }
+    
+    /**
+     * 生成8个随机数
+     * @return
+     */
+    private int[] generateRandomNumber() {
+    	// 随机打乱
+    	ArrayList<Integer> def = new ArrayList<Integer>();
+    	for (int i=1; i<9; i++) {
+    		def.add(new Integer(i));
+    	}
+    	int[] shu = new int[def.size()];
+    	Random ran = new Random();
+    	for (int j=0; j<8; j++) {
+        	for (;;) {
+        		if (def.size() == 1) {
+        			shu[j] = def.get(0);
+        			def.remove(0);
+        			break;
+        		}
+        		int index = ran.nextInt(def.size());
+        		if (def.get(index) == (j+1)) {
+        			continue;
+        		}
+        		shu[j] = def.get(index);
+        		def.remove(index);
+        		break;
+        	}
+    	}
+    	return shu;
+    }
+    
+    public void shuffle() {
+    	if (isShuffle) {
+    		return;
+    	}
+    	int[] shu = generateRandomNumber();
+Log.i("Puzzle", "shuffle PuzzlePosition.position [before]: \n" + xxDebug(PuzzlePosition.position));
+    	for (int i=0; i<8; i++) {
+    		// 打乱 position
+    		int[] ori = PuzzlePosition.getXY(i);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    		PuzzlePosition.position[ori[0]][ori[1]] = shu[i];
+    		int[] xy = PuzzlePosition.getXY(shu[i] - 1);
+    		PuzzlePosition.draw[ori[0]][ori[1]] = PuzzlePosition.drawDefalut[xy[0]][xy[1]];
+    	}
+    	for (int i=0; i<8; i++) {
+    		int[] ori = PuzzlePosition.getXY(i);
+    		ImageButton btn = (ImageButton) findViewById(PuzzlePosition.imbtns[ori[0]][ori[1]]);
+    		btn.setImageResource(PuzzlePosition.draw[ori[0]][ori[1]]);
+    	}
+Log.i("Puzzle", "shuffle PuzzlePosition.position [after]: \n" + xxDebug(PuzzlePosition.position));
+    	isShuffle = true;
     }
     
     public void setEventListene() {
-        for (int i=0; i<9; i++) {
-        	ImageButton btn = (ImageButton) findViewById(imbtns[i]);
-        	btn.setId(i+1);
-        	btn.setOnTouchListener(this);
-        }
-    	ImageButton btn = (ImageButton) findViewById(R.id.imageButton12);
-    	btn.setId(12);
-    	btn.setOnTouchListener(this);
+    	for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
+            	ImageButton btn = (ImageButton) findViewById(
+            			PuzzlePosition.imbtns[i][j]);
+            	btn.setTag(i*3 + j + 1);
+            	btn.setOnTouchListener(this);
+        	}
+    	}
+    	ImageButton btn12 = (ImageButton) findViewById(R.id.imageButton12);
+    	btn12.setTag(12);
+    	btn12.setOnTouchListener(this);
     }
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
-		float inX = 0, inY = 0, x = 0, y = 0;
     	int btnWidth = v.getWidth();
     	int btnHeight = v.getHeight();
 		int eventaction = event.getAction();
@@ -58,63 +109,77 @@ public class MainActivity extends Activity implements OnTouchListener {
 		switch (eventaction) {
 		// 接触屏幕
 		case MotionEvent.ACTION_DOWN:
-			inX = x = event.getX();
-			inY = y = event.getY();
-			Log.i("Puzzle", "ACTION_DOWN: " + x + " - " + y);
+			xDown = event.getRawX();
+			yDown = event.getRawY();
 			break;
 		// 离开屏幕
 		case MotionEvent.ACTION_UP:
-			float horizontalMovement = event.getX() - inX;
-			float verticalMovement = event.getY() - inY;
+			float horizontalMovement = event.getRawX() - xDown;
+			float verticalMovement = event.getRawY() - yDown;
 			int direction = 0;
-			if (horizontalMovement > 0 && horizontalMovement > btnWidth/3.0) {
+//Log.i("Puzzle", "finger up, xDown: " + xDown + ", yDown: " + yDown
+//		+ ", eventX: " + event.getX() + ", eventY: " + event.getY());
+			if (horizontalMovement > 0 && horizontalMovement > btnWidth/3.5) {
 				direction = PuzzlePosition.RIGHT;
 				Log.i("Puzzle", "ACTION_UP: 向右移动");
 			} else if (horizontalMovement < 0 && Math.abs(horizontalMovement) > btnWidth/2.0) {
 				direction = PuzzlePosition.LEFT;
 				Log.i("Puzzle", "ACTION_UP: 向左移动");
-			}
-			if (verticalMovement > 0 && verticalMovement > btnHeight/3.0) {
+			} else if (verticalMovement > 0 && verticalMovement > btnHeight/3.5) {
 				direction = PuzzlePosition.DOWN;
 				Log.i("Puzzle", "ACTION_UP: 向下移动");
 			} else if (verticalMovement < 0 && Math.abs(verticalMovement) > btnHeight/2.0) {
 				direction = PuzzlePosition.UP;
 				Log.i("Puzzle", "ACTION_UP: 向上移动");
 			}
+Log.i("Puzzle", "tag: " + ((Integer)v.getTag()).intValue());
+//Log.i("Puzzle", "direction: " + direction);
+			// 把index转换成x, y坐标
+			int[] xy = PuzzlePosition.getXY(((Integer)v.getTag()).intValue() - 1);
+			int targetIndex = PuzzlePosition.getTargetIndex(xy, direction);
+			int[] targetXY = PuzzlePosition.getXY(targetIndex - 1);
 			
-			int[] xy = PuzzlePosition.getXY(v.getId()-1);
-			
-			Log.i("Puzzle", "id: " + v.getId() + ", xy: " + xy[0] + ", " + xy[1]);
-			Log.i("Puzzle", x + " | " + y + " | " + 
-					horizontalMovement + " | " + verticalMovement + " | " +
-					btnWidth + " | " + btnHeight);
-			Log.i("Puzzle", "canMoveDirection: " + 
-					PuzzlePosition.canMoveDirection(xy, direction) + ", " +
-					PuzzlePosition.getIndex(xy, direction));
-			
-			int [] states = v.getDrawableState();
-			for (int i=0; i<states.length; i++) {
-				Log.i("Puzzle", "drable state: " + String.valueOf(states[i]));
-			}
+Log.i("Puzzle", "canMove: "+PuzzlePosition.canMoveDirection(xy, direction));
+Log.i("Puzzle", "targetIndex: "+targetIndex);
+Log.i("Puzzle", "xy[0]: " + xy[0] + ", xy[1]: " + xy[1] 
+		+ ", imbtns[x][y]: " + PuzzlePosition.imbtns[xy[0]][xy[1]]);
 			
 			if (PuzzlePosition.canMoveDirection(xy, direction)) {
+Log.i("Puzzle", "targetXY[0]: " + targetXY[0] + ", targetXY[1]: " + targetXY[1] 
+		+ ", imbtns[targetXY[0]][targetXY[1]]: " + PuzzlePosition.imbtns[targetXY[0]][targetXY[1]]);
 				// 交换两个小图块的图片
-				Resources res=getResources();
-				int img = res.getIdentifier("part_0" + v.getId(), "drawable", 
-						getPackageName());
-				int sbtnId = res.getIdentifier(
-						"imageButton"+PuzzlePosition.getIndex(xy, direction), 
-						"id", getPackageName());
-				ImageButton sbtn = (ImageButton) findViewById(
-						imbtns[PuzzlePosition.getIndex(xy, direction)-1]);
+				ImageButton targetBtn = (ImageButton) findViewById(
+						PuzzlePosition.imbtns[targetXY[0]][targetXY[1]]);
+				targetBtn.setImageResource(
+						PuzzlePosition.draw[xy[0]][xy[1]]);
 				
-				Log.i("Puzzle", "imageButton"+PuzzlePosition.getIndex(xy, direction) + 
-						", sbtn: " + sbtnId + ", ib12: " + R.id.imageButton12 + ", " + 
-						sbtn);
-//				if (sbtn != null) {
-//				}
-//				sbtn.setImageResource(img);
+				ImageButton btn = (ImageButton) findViewById(
+						PuzzlePosition.imbtns[xy[0]][xy[1]]);
+				btn.setImageResource(
+						PuzzlePosition.draw[targetXY[0]][targetXY[1]]);
+				
+Log.i("Puzzle", targetBtn + ", " + btn);
+				
 				// 交换位置信息
+				int swap;
+//Log.i("Puzzle", "btn ids [before]: \n" + xxDebug(PuzzlePosition.imbtns));
+//				swap = PuzzlePosition.imbtns[xy[0]][xy[1]];
+//				PuzzlePosition.imbtns[xy[0]][xy[1]] = PuzzlePosition.imbtns[targetXY[0]][targetXY[1]];
+//				PuzzlePosition.imbtns[targetXY[0]][targetXY[1]] = swap;
+//Log.i("Puzzle", "btn ids [after]: \n" + xxDebug(PuzzlePosition.imbtns));
+
+Log.i("Puzzle", "draw [before]: \n" + xxDebug(PuzzlePosition.draw));
+				swap = PuzzlePosition.draw[xy[0]][xy[1]];
+				PuzzlePosition.draw[xy[0]][xy[1]] = 
+						PuzzlePosition.draw[targetXY[0]][targetXY[1]];
+				PuzzlePosition.draw[targetXY[0]][targetXY[1]] = swap;
+Log.i("Puzzle", "draw [after]: \n" + xxDebug(PuzzlePosition.draw));
+
+Log.i("Puzzle", "PuzzlePosition.position [before]: \n" + xxDebug(PuzzlePosition.position));
+				swap = PuzzlePosition.position[xy[0]][xy[1]];
+				PuzzlePosition.position[xy[0]][xy[1]] = PuzzlePosition.position[targetXY[0]][targetXY[1]];
+				PuzzlePosition.position[targetXY[0]][targetXY[1]] = swap;
+Log.i("Puzzle", "PuzzlePosition.position [after]: \n" + xxDebug(PuzzlePosition.position));
 			}
 			
 			break;
@@ -122,5 +187,21 @@ public class MainActivity extends Activity implements OnTouchListener {
 		
 		return false;
 	}
-    
+
+public String xxDebug(int[][] c) {
+	String out = "";
+	for (int i=0; i<4; i++) {
+		for (int j=0; j<3; j++) {
+			out += c[i][j] + ", ";
+		}
+		out += "\n";
+	}
+	return out;
+}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		android.os.Process.killProcess(android.os.Process.myPid());
+	}
 }
--- src/kevin/jiang/puzzle/PuzzlePosition.java
+++ src/kevin/jiang/puzzle/PuzzlePosition.java
@@ -11,6 +11,13 @@ public class PuzzlePosition {
 	public final static int RIGHT = 2;
 	public final static int DOWN = 3;
 	public final static int LEFT = 4;
+
+	public static int[][] imbtns = {
+			{R.id.imageButton1, R.id.imageButton2, R.id.imageButton3}, 
+    		{R.id.imageButton4, R.id.imageButton5, R.id.imageButton6}, 
+    		{R.id.imageButton7, R.id.imageButton8, R.id.imageButton9}, 
+    		{R.id.imageButton10, R.id.imageButton11, R.id.imageButton12}
+    };
 	
 	/**
 	 * 默认的小图块的位置
@@ -20,6 +27,29 @@ public class PuzzlePosition {
 			{4, 5, 6}, 
 			{7, 8, 9}, 
 			{-1, -1, 0}};
+
+	public static int[][] positionDefault = {
+			{1, 2, 3}, 
+			{4, 5, 6}, 
+			{7, 8, 9}, 
+			{-1, -1, 0}};
+
+	public static int[][] draw = {
+		{R.drawable.part_01, R.drawable.part_02, R.drawable.part_03},
+		{R.drawable.part_04, R.drawable.part_05, R.drawable.part_06}, 
+		{R.drawable.part_07, R.drawable.part_06, R.drawable.part_09}, 
+		{android.R.drawable.screen_background_light_transparent, 
+			android.R.drawable.screen_background_light_transparent, 
+			android.R.drawable.screen_background_dark_transparent}, 
+	};
+	public static int[][] drawDefalut = {
+		{R.drawable.part_01, R.drawable.part_02, R.drawable.part_03},
+		{R.drawable.part_04, R.drawable.part_05, R.drawable.part_06}, 
+		{R.drawable.part_07, R.drawable.part_08, R.drawable.part_09}, 
+		{android.R.drawable.screen_background_light_transparent, 
+			android.R.drawable.screen_background_light_transparent, 
+			android.R.drawable.screen_background_dark_transparent}, 
+	};
 	
 	/**
 	 * 判断一个小图块是否可以朝指定方向移动
@@ -57,7 +87,7 @@ public class PuzzlePosition {
 	 * @param o
 	 * @return
 	 */
-	public static int getIndex(int[] pos, int o) {
+	public static int getTargetIndex(int[] pos, int o) {
 		int index = 0;
 		if (o == UP) {
 			index = (pos[0]-1)*3 + pos[1] + 1;
